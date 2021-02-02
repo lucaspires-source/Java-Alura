80d0c2ce621b850d176a54a19068f68548581649
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-02 11:31:47 -0300

    Fim da Aula 05.01

---
+++ sintaxe/TestaCaracteres.java
@@ -0,0 +1,16 @@
+package sintaxe;
+
+public class TestaCaracteres {
+    public static void main(String[] args) {
+		char letra = 'a';
+        System.out.println(letra);
+        
+        //Vai sair A
+        char valor = 65 ;
+        System.out.println(valor);
+        
+        //Vai sair B
+		valor = (char) (valor+1);
+		System.out.println(valor);
+	}
+}
