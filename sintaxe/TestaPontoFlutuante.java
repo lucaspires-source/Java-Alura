f423f617f157b72e5b8cd82253abb313f1834b0b
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-02 10:46:09 -0300

    Fim da Aula 04.04

---
+++ sintaxe/TestaPontoFlutuante.java
@@ -0,0 +1,18 @@
+package sintaxe;
+
+public class TestaPontoFlutuante {
+    public static void main(String[] args) {
+        double salario = 2500.70;
+        
+        System.out.println("O meu salário é " + salario);
+
+        double divisao = 3.14 / 2;
+        System.out.println(divisao);
+       
+        int outraDivisao = 5/2;
+        System.out.println(outraDivisao);
+
+        double maisDivisao = 5.0 / 2;
+        System.out.println(maisDivisao);
+	}
+}
