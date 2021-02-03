3ee260dfcdfff1f85e4e51d0f2fc79896a0211c0
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-03 08:35:51 -0300

    Fim da Aula 06.04

---
+++ sintaxe/TestaCondicional2.java
@@ -0,0 +1,16 @@
+package sintaxe;
+
+public class TestaCondicional2 {
+	public static void main(String[] args) {
+		int idade = 20;
+		boolean acompanhado = true;
+		
+		
+		
+		if (idade >= 18 && acompanhado) {
+			System.out.println("Seja bem vindo");
+		}else {
+			System.out.println("Infelizmente você não pode entrar");
+		}
+}}
+
