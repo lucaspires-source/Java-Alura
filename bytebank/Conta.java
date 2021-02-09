d8792b59a4895d8153e05cb7b93abde5aea4f150
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-09 09:30:32 -0300

    fim da aula 03.08

--- bytebank/Conta.java
+++ bytebank/Conta.java
@@ -10,13 +10,24 @@ public class Conta {
 
 
     public void deposita(double valor) {
-        this.saldo = this.saldo + valor;
+        this.saldo  += valor;
     }
 
     public boolean saca (double valor) {
 	    	
         if (this.saldo >= valor) {
-            this.saldo = this.saldo - valor;
+            this.saldo  -= valor;
+            return true;
+        }else {
+            System.out.println("Saldo insuficiente");
+            return false;
+        }
+    }
+
+    public boolean transfere(double valor, Conta destino){
+        if (this.saldo >= valor) {
+            this.saldo  -= valor;
+            destino.deposita(valor);
             return true;
         }else {
             System.out.println("Saldo insuficiente");
--- bytebank/TestaMetodo.java
+++ bytebank/TestaMetodo.java
@@ -4,10 +4,17 @@ public class TestaMetodo {
 	public static void main(String[] args) {
 		Conta contaDoLucas = new Conta();
 		contaDoLucas.saldo = 100;
+        Conta contaDoNeymar = new Conta();
+        contaDoNeymar.saldo = 1000;
+
 		contaDoLucas.deposita(50);
 		
 		System.out.println(contaDoLucas.saldo);
-        contaDoLucas.saca(300);
+        contaDoLucas.saca(150);
+        System.out.println(contaDoLucas.saldo);
+
+
+        contaDoNeymar.transfere(500, contaDoLucas);
         System.out.println(contaDoLucas.saldo);
 	}
 }
