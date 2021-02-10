a645b8ce677b4ee3d0b8e11184aba5b0aa17de9e
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-10 08:52:09 -0300

    fim da aula 05.01

--- bytebank2/Conta.java
+++ bytebank2/Conta.java
@@ -2,36 +2,49 @@ package bytebank2;
 
 
 public class Conta {
-    double saldo; 
+    private double saldo; 
     int agencia;
     int numero;
     Cliente titular;
 
 
 
-    public void deposita(double valor) {
-        this.saldo  += valor;
+    public boolean deposita(double valor) {
+        if(valor > 0){
+            this.saldo  += valor;
+            return true;
+
+        }else{
+            System.out.println("Infelizmente você não pode realizar a transação");
+            return false;
+        }
     }
 
     public boolean saca (double valor) {
 	    	
-        if (this.saldo >= valor) {
+        if (this.saldo >= valor && 0 < valor) {
             this.saldo  -= valor;
             return true;
         }else {
-            System.out.println("Saldo insuficiente");
+           System.out.println("Infelizmente você não pode realizar a transação");
             return false;
         }
     }
 
     public boolean transfere(double valor, Conta destino){
-        if (this.saldo >= valor) {
+        if (this.saldo >= valor && 0 < valor) {
             this.saldo  -= valor;
             destino.deposita(valor);
             return true;
         }else {
-            System.out.println("Saldo insuficiente");
+        	System.out.println("Infelizmente você não pode realizar a transação");
             return false;
         }
     }
+
+
+public double pegaSaldo() {
+    return this.saldo;
+    
+}
 }
---
+++ bytebank2/TestaSacaNegativo.java
@@ -0,0 +1,9 @@
+package bytebank2;
+public class TestaSacaNegativo {
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.deposita(100);
+		conta.saca(-200);
+        System.out.println(conta.pegaSaldo());
+	}
+}
